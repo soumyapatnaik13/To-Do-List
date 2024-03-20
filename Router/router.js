b63a3ea5da9ac3f6e58db144437d1f7b9e5e80b9
Soumya Patnaik <141363016+soumyapatnaik13@users.noreply.github.com>
2024-03-20 12:22:59 -0700

    Update router.js
    path file changed

--- Router/router.js
+++ Router/router.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const router = express.Router();
-const controller= require("../Controller/controller.js")
+const controller= require("../Controller/controller")
 
 router.post("/tasks",controller.createList)
 
